81cad627d3c7cc77877a98b62f2011de60b01b2f
chenhuaxu <chenhuaxu@huobi.com>
2019-11-29 15:32:48 +0800

    修改配置

--- deploy.sh
+++ deploy.sh
@@ -222,7 +222,7 @@ commit+push() {
   if [ $GH_TOKEN ]; then
     # deploy by Travis CI
     # add github token
-    repo="https://"$GH_TOKEN"@github.com/HelloKittySay/test-slate.git"
+    repo="https://"$GH_TOKEN"@github.com/alphaex-api/openapi.git"
     git remote add origin-pages $repo
     git push --quiet origin-pages $deploy_branch
   else
